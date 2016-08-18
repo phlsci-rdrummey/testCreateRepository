c3d3bd8f86268a35d6fd1fa57c14aac0438ec262
Robert Drummey <robertdrummey@phlsci.com>
2016-08-18 09:24:00 -0400

    Add files via upload
    Add template files.

---
+++ template.h
@@ -0,0 +1,23 @@
+/*
+ * template.h
+ * This is the header file for template.c
+ * This contains defines and function prototypes
+ */
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+/*
+ * defines
+ */
+  
+/*
+ * global variables
+ */
+   
+/*
+ * public function prototypes
+ */
+
+
+#endif // TEMPLATE_H
